a54ea7c211ffa63cb0cd2b4e9fae912fab893723
YoritaXP <m-e-l-o-n@outlook.jp>
2018-06-25 02:45:44 +0900

    [Add] config file

--- src/address_gen.py
+++ src/address_gen.py
@@ -1,47 +1,59 @@
-#encoding:utf-8
+# encoding:utf-8
 import hashlib
 import ctypes
 import ctypes.util
 import sys
-import time
+import ConfigParser
 import base58_encode as b58
 
-target1 = 'XXXXXX'
-target2 = 'XXXXXXX'
+inifile = ConfigParser.SafeConfigParser()
+inifile.read('../config.ini')
+target = inifile.get('setting', 'target')
+count_max = inifile.getint('setting', 'count')
+version = inifile.getint('setting', 'version')
+
+print('[setting]')
+print('target : ' + target)
+print('prefix : ' + str(version))
+print('Find ' + str(count_max) + ' address')
+print('Search Start!!')
 
 ssl = ctypes.cdll.LoadLibrary (ctypes.util.find_library ('ssl') or 'libeay32')
 
-while True:
-    #sslでsecp256k1の新しい鍵を作る
+count = 0
+
+while count < count_max:
+    # sslでsecp256k1の新しい鍵を作る
     ssl.EC_KEY_new_by_curve_name.restype = ctypes.c_void_p
     secp256k1 = ssl.EC_KEY_new_by_curve_name(714)
     ssl.EC_KEY_generate_key(secp256k1)
     
-    #アドレス用のpubkeyを得る
+    # get pubkey
     size = ssl.i2o_ECPublicKey(secp256k1, 0)
     mb = ctypes.create_string_buffer(size)
     ssl.i2o_ECPublicKey(secp256k1, ctypes.byref(ctypes.pointer(mb)))
     pubkey = mb.raw.rjust(32, chr(0))
     
-    #秘密鍵を得る
+    # get privkey
     bn = ssl.EC_KEY_get0_private_key(secp256k1);
     bytes = (ssl.BN_num_bits(bn) + 7) / 8
     mb = ctypes.create_string_buffer(bytes)
     n = ssl.BN_bn2bin(bn, mb)
     secret = mb.raw
     
-    #アドレスをsha256でダイジェスト取得してさらにRIPEMD-160でダイジェスト取得
+    # アドレスをsha256でダイジェスト取得してさらにRIPEMD-160でダイジェスト取得
     h1 = hashlib.new('ripemd160')
     h1.update(hashlib.sha256(pubkey).digest())
     hash160 = h1.digest()
 
-    #base58にエンコード
-    version = 75
+    # base58 encode
     address = b58.base58_encode(hash160, version)
     secretkey = b58.base58_encode(secret, 128+version)
 
-    if address.startswith(target1):
+    if address.startswith(target):
         print(address,secretkey)
-    if address.startswith(target2):
-        print(address,secretkey)
+        count += 1
+
     ssl.EC_KEY_free(secp256k1)
+
+print('Search End!!')
